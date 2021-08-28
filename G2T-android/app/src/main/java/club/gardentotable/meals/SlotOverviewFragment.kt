8c4daa4d7b3b5158259c0d20979883203e2a7d0c
Ian B <ibansenauer@gmail.com>
2021-08-27 17:13:52 -0700

    added clickable buttons to overview screen and streamlined text

--- G2T-android/app/src/main/java/club/gardentotable/meals/SlotOverviewFragment.kt
+++ G2T-android/app/src/main/java/club/gardentotable/meals/SlotOverviewFragment.kt
@@ -1,31 +1,39 @@
 package club.gardentotable.meals
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.commit
+import androidx.fragment.app.replace
 import club.gardentotable.meals.databinding.FragmentSlotOverviewBinding
 import club.gardentotable.meals.db.Slot
-
-import club.gardentotable.meals.ui.MemberViewModel
-import club.gardentotable.meals.ui.SlotDetailDialogFragment
+import club.gardentotable.meals.ui.*
 import java.time.LocalDate
 
 class SlotOverviewFragment : Fragment() {
     private var _binding : FragmentSlotOverviewBinding? = null
     val binding get() = _binding!!
-    private val memberViewModel: MemberViewModel by activityViewModels()
-    private lateinit var slotDays:List<TextView?>
-    private lateinit var slotNames : List<TextView?>
+    private val slotViewModel: SlotViewModel by activityViewModels{
+        SlotViewModelFactory((requireActivity().application as SignupApplication).repository) }
+    private val memberViewModel: MemberViewModel by activityViewModels {
+        MemberViewModelFactory(((requireActivity().application as SignupApplication).repository)) }
+    private lateinit var overviewButtons : List<Button>
+    private lateinit var slotNames : List<TextView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setHasOptionsMenu(true)
 
     }
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -33,38 +41,66 @@ class SlotOverviewFragment : Fragment() {
     ): View {
         super.onCreateView(inflater, container, savedInstanceState)
         _binding = FragmentSlotOverviewBinding.inflate(inflater, container, false)
-        slotDays = listOf(binding.slot1Name, binding.slot2Name, binding.slot3Name, binding.slot4Name)
-        slotNames = listOf(binding.slot1Day, binding.slot2Day, binding.slot3Day, binding.slot4Day)
+        overviewButtons = listOf(binding.slot1, binding.slot2, binding.slot3, binding.slot4)
+        slotNames = listOf(binding.slot1Name, binding.slot2Name, binding.slot3Name, binding.slot4Name)
         return binding.root
     }
+    private fun onUpdateView(slots: List<Slot>) {
+        val monthlySlotCount : Int
+        val testDate = LocalDate.of(2020,5,1)
 
-     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        var monthlySlotCount : Int
-        super.onViewCreated(view, savedInstanceState)
-         val testDate = LocalDate.of(2020,5,1)
-         var assignedMonthlySlots : List<Slot>
+        slotViewModel.export(requireContext())
+        val assignedMonthlySlots : List<Slot> = slots.filter { slot ->
+            //TODO: testDate should be LocalDate.now.month
+            slot.date.month == testDate.month
 
-         binding.filledSlots = "0/4"
-         memberViewModel.currentUserSlots.observe(viewLifecycleOwner, { slots ->
-             assignedMonthlySlots = slots.filter { slot ->
-                 //TODO: testDate should be LocalDate.now.month
-                 slot.date.month == testDate.month
+        }
+        Log.i("SLOTS", slots.size.toString())
 
-             }
-             //only update UI if the monthly slots is not empty
-            if(assignedMonthlySlots.isNotEmpty()) {
+        //only update UI if the monthly slots is not empty
+        binding.filledSlots = "0/4"
+        overviewButtons.forEach {
+            it.setOnClickListener  { parentFragmentManager.commit {
+            this.replace<SlotFragment>(R.id.fragmentContainerView)
+            this.addToBackStack("slotsView")
+        }}
+            it.text = ""
+        }
+        slotNames.forEach { it.text = "" }
+        binding.signupTracker.monthlySlots = 0
+        if(assignedMonthlySlots.isNotEmpty()) {
 
-                monthlySlotCount = assignedMonthlySlots.count()
-                binding.signupTracker.monthlySlots = monthlySlotCount
-                binding.filledSlots = requireContext().getString(R.string.display_date, monthlySlotCount.toString(), "4")
+            monthlySlotCount = assignedMonthlySlots.count()
+            binding.signupTracker.monthlySlots = monthlySlotCount
+            binding.filledSlots = requireContext().getString(R.string.display_date, monthlySlotCount.toString(), "4")
 
 
-                for (i in slotDays.indices)
-                    if (monthlySlotCount > i) {
-                        slotNames[i]?.text = assignedMonthlySlots[i].task.toString()
-                        slotDays[i]?.text = assignedMonthlySlots[i].day.toString()
+            for (i in overviewButtons.indices)
+                if (monthlySlotCount > i) {
+                    val currentSlot = assignedMonthlySlots[i]
+                    overviewButtons[i].text = requireContext().getString(R.string.overview_text_day, currentSlot.date.monthValue.toString(), currentSlot.date.dayOfMonth.toString(), currentSlot.day.toString())
+                   slotNames[i].text =  currentSlot.task.toString()
+
+                    Log.i("COUNT", i.toString())
+
+                    overviewButtons[i].setOnClickListener{
+                        SlotDetailDialogFragment(currentSlot, slotViewModel).show(parentFragmentManager, "detail")
                     }
-            }
+                }
+
+        }
+
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        super.onViewCreated(view, savedInstanceState)
+
+
+         memberViewModel.currentUserSlots.observe(viewLifecycleOwner, { slots ->
+             memberViewModel.export(requireContext())
+             slots?.let { onUpdateView(it) }
          })
 
 
@@ -72,4 +108,7 @@ class SlotOverviewFragment : Fragment() {
 
 
 
+
 }
+
+
